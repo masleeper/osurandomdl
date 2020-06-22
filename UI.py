bbbdaf229ea6b1496e118590857f9a6f72bfc6da
Vincent Ma <vincent.ma810@gmail.com>
2020-06-22 17:12:39 -0600

    Finish parameters selection UI

--- UI.py
+++ UI.py
@@ -1,17 +1,77 @@
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
 from SlideEdit import SlideEdit
 
 class MainWindow(QMainWindow):
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.setWindowTitle("My Awesome App")
-        sl = SlideEdit()
+        self.setWindowTitle("Osu Random Map Downloader")
 
-        layout = QHBoxLayout()
-        layout.addWidget(sl)
+        mainLayout = QVBoxLayout()
+        self.lengthRow = self.lineEditRow("Length")
+        self.bpmRow = self.lineEditRow("BPM")
+        self.minYearRow = self.lineEditRow("Minimum Year")
+        self.maxYearRow = self.lineEditRow("Maximum Year")
+        self.csRow = self.slideEditRow("Circle Size")
+        self.hpRow = self.slideEditRow("HP")
+        self.odRow = self.slideEditRow("Overall Difficulty")
+        self.arRow = self.slideEditRow("Approach Rate")
+        self.srRow = self.slideEditRow("Star Rating")
+        self.findButton = QPushButton("Search")
+
+        self.findButton.pressed.connect(self.validate)
+
+        mainLayout.addWidget(self.lengthRow)
+        mainLayout.addWidget(self.bpmRow)
+        mainLayout.addWidget(self.minYearRow)
+        mainLayout.addWidget(self.maxYearRow)
+        mainLayout.addWidget(self.csRow)
+        mainLayout.addWidget(self.hpRow)
+        mainLayout.addWidget(self.odRow)
+        mainLayout.addWidget(self.arRow)
+        mainLayout.addWidget(self.srRow)
+        mainLayout.addWidget(self.findButton)
+
+        mainWidget = QWidget()
+        mainWidget.setLayout(mainLayout)
+        self.setCentralWidget(mainWidget)
+
+    def lineEditRow(self, text):
+        hLayout = QHBoxLayout()
+        label = QLabel(str(text))
+        lineEdit = QLineEdit()
+        includeRadButton = QRadioButton("include")
+
+        includeRadButton.setChecked(True)
+
+        hLayout.addWidget(label)
+        hLayout.addWidget(lineEdit)
+        hLayout.addWidget(includeRadButton)
+
+        hLayout.setAlignment(Qt.AlignRight)
+
         widget = QWidget()
-        widget.setLayout(layout)
-        self.setCentralWidget(widget)
+        widget.setLayout(hLayout)
+        return widget
 
+    def slideEditRow(self, text):
+        hLayout = QHBoxLayout()
+        hLayout.setAlignment(Qt.AlignRight)
+        label = QLabel(str(text))
+        slideEdit = SlideEdit()
+        includeRadButton = QRadioButton("nclude")
+
+        slideEdit.setAlignment(Qt.AlignRight)
+        includeRadButton.setChecked(True)
+
+        hLayout.addWidget(label)
+        hLayout.addWidget(slideEdit)
+        hLayout.addWidget(includeRadButton)
+
+        widget = QWidget()
+        widget.setLayout(hLayout)
+        return widget
+
+    def validate(self):
+        print("Pushed")
