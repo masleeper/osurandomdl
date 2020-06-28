f6e0c3d09a31723a72378b8c150f497d11713efe
Vincent Ma <vincent.ma810@gmail.com>
2020-06-28 02:09:50 -0600

    Stub out other game modes

---
+++ ManiaOptions.py
@@ -0,0 +1,12 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
+
+class ManiaOptions(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        mainLayout = QVBoxLayout()
+        label = QLabel("Not yet available")
+        mainLayout.addWidget(label)
+        mainLayout.setAlignment(Qt.AlignCenter)
+        self.setLayout(mainLayout)
