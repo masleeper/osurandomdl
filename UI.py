0e4668794eb6ac04f31f01046165133035567b3f
Vincent Ma <vincent.ma810@gmail.com>
2020-06-28 02:26:21 -0600

    Add map display

--- UI.py
+++ UI.py
@@ -3,9 +3,7 @@ from StdOptions import StdOptions
 from ManiaOptions import ManiaOptions
 from CtbOptions import CtbOptions
 from TaikoOptions import TaikoOptions
-from PyQt5.QtCore import Qt
-from SlideEdit import SlideEdit
-import APICaller
+from MapDisplay import MapDisplay
 
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
@@ -14,17 +12,22 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Osu Random Map Downloader")
 
         mainLayout = QHBoxLayout()
+
+        self.tabs = QTabWidget()
+        self.mapDisplay = MapDisplay()
+
+        # tabs for each game mode
         self.stdOptions = StdOptions()
         self.maniaOptions = ManiaOptions()
         self.ctbOptions = CtbOptions()
         self.taikoOptions = TaikoOptions()
-
-        self.tabs = QTabWidget()
         self.tabs.addTab(self.stdOptions, "Standard")
         self.tabs.addTab(self.maniaOptions, "Mania")
         self.tabs.addTab(self.ctbOptions, "Catch the Beat")
         self.tabs.addTab(self.taikoOptions, "Taiko")
+
         mainLayout.addWidget(self.tabs)
+        mainLayout.addWidget(self.mapDisplay)
 
         mainWidget = QWidget()
         mainWidget.setLayout(mainLayout)
